92b1c09d564ef0a77e63076904b35830b9754a7d
Dmitry Grodo <>
2024-07-14 17:41:35 +0300

    Removed unused namespaces

--- Assets/GunRun/Scripts/Weapons/Pistol.cs
+++ Assets/GunRun/Scripts/Weapons/Pistol.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace GunRun.Scripts
 {
     public class Pistol : ShootingWeapon
--- Assets/GunRun/Scripts/Weapons/Shotgun.cs
+++ Assets/GunRun/Scripts/Weapons/Shotgun.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace GunRun.Scripts
 {
     public class Shotgun : ShootingWeapon
